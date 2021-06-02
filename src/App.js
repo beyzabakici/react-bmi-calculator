4eb45bdf0ae3037d0ac9dd2387093f5ecc5b14ff
Melike Beyza BAKICI <46328844+beyzabakici@users.noreply.github.com>
2021-06-02 11:47:47 +0300

    Improvement bmi calculator

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import Form from  './components/Form';
 function App() {
   const [height, setHeight] = React.useState('');
   const [weight, setWeight] = React.useState('');
+  const [bmi, setBmi] = React.useState();
 
   const heightInputChange = (e) => {
     const newHeight = e.target.value;
     setHeight(newHeight);
-    console.log(newHeight);
   };
 
 const weightInputChange = (e) => {
     const newWeight = e.target.value;
     setWeight(newWeight);
-    console.log(newWeight);
   };
 
 function appTitle() {
   return <h1 className='App-title'>BODY MASS INDEX</h1>
+}
+
+function bmiCalculate(){
+  const newBmi = Math.floor(weight / Math.pow(height, 2));
+  setBmi(newBmi);
+  console.log(bmi);
 }
 
   return (
@@ -30,7 +35,7 @@ function appTitle() {
         onHeightInputChange={(e) => heightInputChange(e)} 
         weightInput={weight}
         onWeightInputChange={(e) => weightInputChange(e)}
-        addValue={() => {}}
+        addValue={() => bmiCalculate()}
       />
     </div>  
   );
