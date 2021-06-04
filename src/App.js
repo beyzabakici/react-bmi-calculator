121acb1ec19a47d01e1141ac21ae9e6110618ae2
Melike Beyza BAKICI <46328844+beyzabakici@users.noreply.github.com>
2021-06-04 13:27:36 +0300

    Complated form components

--- src/App.js
+++ src/App.js
@@ -3,52 +3,15 @@ import './App.css';
 import Form from './components/Form/Form';
 
 function App() {
-  const [height, setHeight] = React.useState('');
-  const [weight, setWeight] = React.useState('');
-  const [bmi, setBmi] = React.useState();
-  const [remember, setRemember] = React.useState(false);
-
-  const heightInputChange = (e) => {
-    setHeight(e.target.value);
-  };
-
-  const weightInputChange = (e) => {
-    setWeight(e.target.value);
-  };
 
   function appTitle() {
     return <h1 className='App-title'>BODY MASS INDEX</h1>
   }
 
-  function bmiCalculate() {
-    const newBmi = Math.floor(weight / Math.pow(height, 2) * 10000);
-    setBmi(newBmi);
-    console.log(bmi);
-  }
-
-  function handleRemember() {
-    setRemember(!remember)
-  }
-
   return (
     <div className="App container">
       {appTitle()}
-      <Form
-        heightInput={
-          remember ? JSON.parse(localStorage.getItem('userHeight')) : height
-        }
-        onHeightInputChange={(e) => {
-          remember ? setHeight(localStorage.getItem('userHeight')) :  heightInputChange(e);
-        }}
-        weightInput={
-          remember ? JSON.parse(localStorage.getItem('userWeight')) : weight
-        }
-        onWeightInputChange={(e) => {
-          remember ? setWeight(localStorage.getItem('userWeight')) :  weightInputChange(e);
-        }}
-        addValue={() => bmiCalculate()}
-        rememberData={() => handleRemember()}
-      />
+      <Form />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,18 +2,52 @@ import React from 'react';
 import './Form.css';
 import RememberCheckbox from '../RememberCheckbox/RememberCheckbox';
 
-export default function Form({ heightInput, onHeightInputChange, weightInput, onWeightInputChange, addValue, rememberData }) {
+export default function Form() {
+  const [height, setHeight] = React.useState('');
+  const [weight, setWeight] = React.useState('');
+  const [bmi, setBmi] = React.useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
-  function writeUserData() {
-    if (!heightInput.trim() && !weightInput.trim()) {
-      return 
-    }  
-    localStorage.setItem('userHeight', JSON.stringify(heightInput));
-    localStorage.setItem('userWeight', JSON.stringify(weightInput));
+  const handleRememberChecked = (e) => {
+    let initalWeight = 0;
+    let initialHeight = 0;
+
+    if (e) {
+      initalWeight = localStorage.getItem('userWeight') || 0;
+      initialHeight = localStorage.getItem('userHeight') || 0;
+    }
+
+    setWeight(initalWeight);
+    setHeight(initialHeight);
+  };
+
+  const heightInputChange = (e) => {
+    setHeight(e.target.value);
+  };
+
+  const weightInputChange = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const bmiCalculate = () => {
+    const calculated = Math.floor(weight / Math.pow(height, 2) * 10000);
+    setBmi(calculated);
+
+    alert(`Body Mass Index: ${bmi}`);
+  }
+
+  const saveUserData = () => {
+    if (!height && !weight) {
+      return;
+    }
+
+    localStorage.setItem('userHeight', height);
+    localStorage.setItem('userWeight', weight);
+
+    bmiCalculate();
   }
 
   return (
@@ -21,28 +55,28 @@ export default function Form({ heightInput, onHeightInputChange, weightInput, on
       <div className='input-container'>
         <div className='input'>
           <input
-            type='number'
+            type="number"
             placeholder='Plase enter to yours weight'
-            value={weightInput}
-            onChange={onWeightInputChange}
+            value={weight}
+            onChange={weightInputChange}
             className="form-control"
           />
         </div>
         <div>
           <input
-            type='number'
+            type="number"
             placeholder='Plase enter to yours height'
-            value={heightInput}
-            onChange={onHeightInputChange}
+            value={height}
+            onChange={heightInputChange}
             className="form-control"
           />
         </div>
       </div>
       <div className='button-container'>
-        <button onClick={addValue, writeUserData} className="btn btn-outline-primary">
+        <button onClick={saveUserData} className="btn btn-outline-primary">
           Calculate !
       </button>
-        <RememberCheckbox isMarked={rememberData}/>
+        <RememberCheckbox isMarked={handleRememberChecked}/>
       </div>
     </form>
   )
